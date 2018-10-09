b1de9d15125fa9d54e91e7ca51767677c82d7da0
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-10-09 21:24:49 +0900

    copy header() from Doku_Renderer_xhtml

--- renderer.php
+++ renderer.php
@@ -29,6 +29,11 @@ class renderer_plugin_linebreak2 extends Doku_Renderer_xhtml {
         $this->_counter = array();
     }
 
+    /**
+     * Render plain text data
+     *
+     * @param string $text  the text to display
+     */
     function cdata($text) {
 
         $html = $this->_xmlEntities($text);
@@ -67,6 +72,55 @@ class renderer_plugin_linebreak2 extends Doku_Renderer_xhtml {
         // Normal mode: identical with the standard xhml renderer
         // leave line break chars as is
         $this->doc .= $html;
+    }
+
+    /**
+     * Render a heading
+     *
+     * @param string $text  the text to display
+     * @param int    $level header level
+     * @param int    $pos   byte position in the original source
+     */
+    function header($text, $level, $pos) {
+        global $conf;
+
+        if(blank($text)) return; //skip empty headlines
+
+        $hid = $this->_headerToLink($text, true); // Creates a linkid from a headline
+
+        //only add items within configured levels
+        $this->toc_additem($hid, $text, $level);
+
+        // adjust $node to reflect hierarchy of levels
+        $this->node[$level - 1]++;
+        if ($level < $this->lastlevel) {
+            for($i = 0; $i < $this->lastlevel - $level; $i++) {
+                $this->node[$this->lastlevel - $i - 1] = 0;
+            }
+        }
+        $this->lastlevel = $level;
+
+        if ($level <= $conf['maxseclevel']
+            && count($this->sectionedits) > 0
+            && $this->sectionedits[count($this->sectionedits) - 1]['target'] === 'section'
+        ) {
+            $this->finishSectionEdit($pos - 1);
+        }
+
+        // write the header
+        $this->doc .= DOKU_LF.'<h'.$level;
+        if ($level <= $conf['maxseclevel']) {
+            $data = [
+                'target' => 'section',
+                'name' => $text,
+                'hid' => $hid,
+                'codeblockOffset' => $this->_codeblock,
+            ];
+            $this->doc .= ' class="'.$this->startSectionEdit($pos, $data).'"';
+        }
+        $this->doc .= ' id="'.$hid.'">';
+        $this->doc .= $this->_xmlEntities($text);
+        $this->doc .= '</h'.$level.'>'.DOKU_LF;
     }
 
 }
